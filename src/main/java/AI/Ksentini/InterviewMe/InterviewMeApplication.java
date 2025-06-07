6847cc0dae7fa6d6a7ed4cee1fe1a37f55c134b7
hamzaksentini <ksentini1999@gmail.com>
2025-06-07 20:30:08 +0100

    starting project

---
+++ src/main/java/AI/Ksentini/InterviewMe/InterviewMeApplication.java
@@ -0,0 +1,13 @@
+package AI.Ksentini.InterviewMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AI/Ksentini/InterviewMe/InterviewMeApplicationTests.java
@@ -0,0 +1,13 @@
+package AI.Ksentini.InterviewMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
